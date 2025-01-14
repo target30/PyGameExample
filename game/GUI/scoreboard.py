5d5babf171d1edfd9c3f8a2be5e803e658039090
Vladimir <korsunvladimir22@mail.ru>
2025-01-14 21:54:55 +0300

    add highest score

--- game/GUI/scoreboard.py
+++ game/GUI/scoreboard.py
@@ -13,12 +13,27 @@ class Scoreboard:
         # Настройки шрифта для вывода
         self.text_color = (30,30,30)
         self.font = pygame.font.SysFont(None, 48)
-        # Подготовка  исходного изображения
+        # Подготовка исходного изображения
         self.prep_score()
+        self.prep_high_score()
+
+    def prep_high_score(self):
+        """Преобразует рекордный счёт в графическое изображение"""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(
+            high_score_str, True,
+            self.text_color, self.settings.bg_color)
+
+        # Рекорд выравнивается по центру верхней стороны
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.screen_rect.top
 
     def prep_score(self):
         """Преобразует текущий счёт в графическое изображение"""
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(
             score_str,True,self.text_color,
             self.settings.bg_color)
@@ -31,3 +46,10 @@ class Scoreboard:
     def show_score(self):
         """Вывод счёта на экран"""
         self.screen.blit(self.score_image,self.score_rect)
+        self.screen.blit(self.high_score_image,self.high_score_rect)
+
+    def check_high_score(self):
+        """Проверяет, появился ли новый рекорд"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
--- game/game.py
+++ game/game.py
@@ -200,6 +200,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points
             self.sb.prep_score()
+            self.sb.check_high_score()
 
     def _check_aliens_bottom(self):
         """Проверяет, добрались ли пришельцы до нижнешл края экрана"""
